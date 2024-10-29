9974c3cb058c90662ad1f2a366b3d5460fd6d233
sgidalevitz <simon.gidalevitz@gmail.com>
2024-10-29 16:01:43 -0500

    Created MoveType class

---
+++ src/MoveGeneration/MoveType.java
@@ -0,0 +1,12 @@
+package MoveGeneration;
+
+public enum MoveType {
+    QUIET,
+    CAPTURE,
+    DOUBLE_PAWN_PUSH,
+    KING_CASTLE,
+    QUEEN_CASTLE,
+    EN_PASSANT,
+    PROMOTION,
+    PROMOTION_CAPTURE
+}
